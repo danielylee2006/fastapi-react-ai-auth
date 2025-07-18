173025e15265fd6ce8d52c7641330bd771c5a68e
Daniel Lee <danielyedarmlee@gmail.com>
2025-07-18 12:53:28 -0400

    set up routers

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,25 @@
-import { useState } from 'react'
-import './App.css'
-import {Routes, Route} from "react-router-dom"
-import {ClerkProviderWithRoutes} from "./auth/ClerkProviderWithRoutes.jsx"
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Layout } from "./layout/Layout.jsx";
+import { ChallengeGenerator } from "./challenge/ChallengeGenerator.jsx";
+import { HistoryPanel, HistoryPannel } from "./history/HistoryPanel.jsx";
+import { AuthenticationPage } from "./auth/AuthenticationPage.jsx";
+import { ClerkProviderWithRoutes } from "./auth/ClerkProviderWithRoutes.jsx";
 
 function App() {
-  return <ClerkProviderWithRoutes>
-  
-  <Routes>
-    
-  </Routes>
+  return (
+    <ClerkProviderWithRoutes>
+      <Routes>
+        <Route path="/sign-in/*" element={<AuthenticationPage />} />
+        <Route path="/sign-up/" element={<AuthenticationPage />} />
 
-  </ClerkProviderWithRoutes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<ChallengeGenerator />} />
+          <Route path="/history" element={<HistoryPannel />} />
+        </Route>
+      </Routes>
+    </ClerkProviderWithRoutes>
+  );
 }
 
-export default App
+export default App;
